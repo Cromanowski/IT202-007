4f7db1543fc2c789492b8ce74b8d15b6cba51729
Your Name <cmr75@njit.edu>
2021-11-09 10:21:05 -0500

    --a

--- public_html/Project/Register.php
+++ public_html/Project/Register.php
@@ -71,7 +71,7 @@ if (isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["confirm
         $stmt = $db->prepare("INSERT INTO Users (email, password, username) VALUES(:email, :password, :username)");
         try {
             $stmt->execute([":email" => $email, ":password" => $hash, ":username" =>$username]);
-            echo "You've registered, yay...";
+            echo " You've registered, yay...";
         } catch (Exception $e) {
             echo "There was a problem registering";
             echo "<pre>" . var_export($e, true) . "</pre>";
